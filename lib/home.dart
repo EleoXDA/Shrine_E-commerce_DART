094076418d355ac47ef707021f9bfd90cf8dcf67
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-21 21:50:25 +0200

    Add an app bar widget

--- lib/home.dart
+++ lib/home.dart
@@ -23,13 +23,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+          // TODO: Add buttons and title (102)
+          ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
       ),
-      // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
     );
   }
