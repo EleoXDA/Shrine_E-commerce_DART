1d043cb4f9d9ac70257ae518554f6a1c0972d5a7
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-21 22:11:39 +0200

    Add column widget to layout vertically + BUGFIX

--- lib/home.dart
+++ lib/home.dart
@@ -65,10 +65,31 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
         // TODO: Build a grid of cards (102)
-        children: <Widget>[Card()],
-      ),
-      body: Center(
-        child: Text('You did it!'),
+        children: <Widget>[
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('assets/diamond.png'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Title'),
+                      const SizedBox(height: 8.0),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
       resizeToAvoidBottomInset: false,
     );
