4fed1c0dfd21946bae2b91625c676362f55931da
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-21 22:08:13 +0200

    Replace the center in the body with a GridView

--- lib/home.dart
+++ lib/home.dart
@@ -60,6 +60,13 @@ class HomePage extends StatelessWidget {
           ]),
 
       // TODO: Add a grid view (102)
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        // TODO: Build a grid of cards (102)
+        children: <Widget>[Card()],
+      ),
       body: Center(
         child: Text('You did it!'),
       ),
