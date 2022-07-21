b393fea6f7c91ec745ecd77c1490ba1e33ace124
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-21 22:03:13 +0200

    Add search button on app bar

--- lib/home.dart
+++ lib/home.dart
@@ -26,18 +26,30 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       // TODO: Add app bar (102)
       appBar: AppBar(
-        // TODO: Add buttons and title (102)
-        leading: IconButton(
-          icon: const Icon(
-            Icons.menu,
-            semanticLabel: 'menu',
+          // TODO: Add buttons and title (102)
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: () {
+              print('Menu button');
+            },
           ),
-          onPressed: () {
-            print('Menu button');
-          },
-        ),
-        title: const Text('SHRINE'),
-      ),
+          title: const Text('SHRINE'),
+          // TODO: Add trailing buttons (102)
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {
+                print('Search button');
+              },
+            ),
+          ]),
+
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
