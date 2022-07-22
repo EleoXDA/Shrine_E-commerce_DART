35242e1a987691609ecba4b0cf5a82b59483edca
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-22 08:44:57 +0200

    Align the text to center of boxes

--- lib/home.dart
+++ lib/home.dart
@@ -52,8 +52,8 @@ class HomePage extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
                 child: Column(
-                  // TODO: Align labels to the bottom and center (103)
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   // TODO: Change innermost Column (103)
                   children: <Widget>[
                     // TODO: Handle overflowing labels (103)
