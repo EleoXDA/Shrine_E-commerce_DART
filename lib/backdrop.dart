6632ab85f7c257a019cd4aef86f87bef4e6fa4d5
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-23 22:10:34 +0200

    Return stack scaffold with app bar from home.dart

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -26,4 +26,60 @@ class Backdrop extends StatefulWidget {
 
 // TODO: Add _FrontLayer class (104)
 // TODO: Add _BackdropTitle class (104)
-// TODO: Add _BackdropState class (104)
+class _BackdropState extends State<Backdrop>
+    with SingleTickerProviderStateMixin {
+  final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
+
+  // TODO: Add AnimationController widget (104)
+
+  // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
+  Widget _buildStack() {
+    return Stack(
+      key: _backdropKey,
+      children: <Widget>[
+        // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
+        widget.backLayer,
+        widget.frontLayer,
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appBar = AppBar(
+      elevation: 0.0,
+      titleSpacing: 0.0,
+      // TODO: Replace leading menu icon with IconButton (104)
+      // TODO: Remove leading property (104)
+      // TODO: Create title with _BackdropTitle parameter (104)
+      leading: Icon(Icons.menu),
+      title: Text('SHRINE'),
+      actions: <Widget>[
+        // TODO: Add shortcut to login screen from trailing icons (104)
+        IconButton(
+          icon: Icon(
+            Icons.search,
+            semanticLabel: 'search',
+          ),
+          onPressed: () {
+            // TODO: Add open login (104)
+          },
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.tune,
+            semanticLabel: 'filter',
+          ),
+          onPressed: () {
+            // TODO: Add open login (104)
+          },
+        ),
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
+      // TODO: Return a LayoutBuilder widget (104)
+      body: _buildStack(),
+    );
+  }
+}
