103de524b6f8ba02de7d6bc699f47371da83e591
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-24 13:34:23 +0200

    Initiate animation controller & dispose afterwards

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -58,7 +58,27 @@ class _BackdropState extends State<Backdrop>
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
 
   // TODO: Add AnimationController widget (104)
+  late AnimationController _controller;
 
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  // TODO: Add override for didUpdateWidget (104)
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // TODO: Add functions to get and change front layer visibility (104)
   // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
   Widget _buildStack() {
     // TODO: Create a RelativeRectTween Animation (104)
