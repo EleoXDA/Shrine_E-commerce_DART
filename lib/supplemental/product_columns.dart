667c6ce23b3e8bf83fd0e7bc46ee33bb37656b70
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-25 08:00:39 +0200

    Replace the column with a listview

--- lib/supplemental/product_columns.dart
+++ lib/supplemental/product_columns.dart
@@ -39,10 +39,8 @@ class TwoProductCardColumn extends StatelessWidget {
           ? constraints.biggest.width / heightOfImages
           : 49.0 / 33.0;
 
-      // TODO: Replace Column with a ListView (104)
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      return ListView(
+        physics: const ClampingScrollPhysics(),
         children: <Widget>[
           Padding(
             padding: const EdgeInsetsDirectional.only(start: 28.0),
