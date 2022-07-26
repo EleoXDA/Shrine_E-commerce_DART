05043c7661ebb0eff4ad6405bb15fe813517a388
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-26 08:04:04 +0200

    QuickFix

--- lib/home.dart
+++ lib/home.dart
@@ -22,7 +22,7 @@ import 'supplemental/asymmetric_view.dart';
 class HomePage extends StatelessWidget {
   final Category category;
   const HomePage({this.category = Category.all, Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return AsymmetricView(
@@ -31,65 +31,64 @@ class HomePage extends StatelessWidget {
   }
 }
 
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
+List<Card> _buildGridCards(BuildContext context) {
+  List<Product> products = ProductsRepository.loadProducts(Category.all);
 
-    if (products.isEmpty) {
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString());
-
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        elevation: 0.0,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18 / 11,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      product.name,
-                      style: theme.textTheme.button,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    const SizedBox(height: 4.0),
-                    Text(
-                      formatter.format(product.price),
-                      style: theme.textTheme.caption,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
-  } // TODO: Add a variable for Category (104)
-
-  @override
-  Widget build(BuildContext context) {
-    return AsymmetricView(
-        products: ProductsRepository.loadProducts(Category.all));
-    // TODO: Pass Category variable to AsymmetricView (104)
+  if (products.isEmpty) {
+    return const <Card>[];
   }
+
+  final ThemeData theme = Theme.of(context);
+  final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString());
+
+  return products.map((product) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 0.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 18 / 11,
+            child: Image.asset(
+              product.assetName,
+              package: product.assetPackage,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    product.name,
+                    style: theme.textTheme.button,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 4.0),
+                  Text(
+                    formatter.format(product.price),
+                    style: theme.textTheme.caption,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }).toList();
+} // TODO: Add a variable for Category (104)
+
+@override
+Widget build(BuildContext context) {
+  return AsymmetricView(
+      products: ProductsRepository.loadProducts(Category.all));
+  // TODO: Pass Category variable to AsymmetricView (104)
 }
