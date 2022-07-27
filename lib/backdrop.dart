a3e9f72fd76b86bcf2ddb97ff697b9961b260e04
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-27 22:33:26 +0200

    OnTap to open&close top layer each time it is pressed

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -128,7 +128,7 @@ class _BackdropState extends State<Backdrop>
         PositionedTransition(
           rect: layerAnimation,
           child: _FrontLayer(
-            // TODO: Implement onTap property on _BackdropState (104)
+            onTap: _toggleBackdropLayerVisibility,
             child: widget.frontLayer,
           ),
         ),
